4527e255ae759a7f0602e7dd1773b9067b2f14ae
Boss <wti12012012@gmail.com>
2021-12-24 09:58:50 +0200

    Jenkins file multi-piped

---
+++ java-maven-app-jenkins-jobs/script.groovy
@@ -0,0 +1,16 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
